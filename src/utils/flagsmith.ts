23f3e50e92f7b35cfed56ecba4129150cbe7b80e
Adam <adam@bonsu.io>
2024-03-23 12:51:29 +0000

    sr

---
+++ src/utils/flagsmith.ts
@@ -0,0 +1,8 @@
+const Flagsmith = require('flagsmith-nodejs');
+
+const flagsmith = new Flagsmith({
+ environmentKey: process.env.FLAGSMITH_KEY as string,
+ requestTimeoutSeconds: 60
+});
+
+export default flagsmith
